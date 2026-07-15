2382436ec8e648a2b42a953579da4bf043347605
zaka13alt <zakariyaliban719@gmail.com>
2026-07-15 13:24:32 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const createBareServer = require('@tomphttp/bare-server-node');
+
+const app = express();
+const server = http.createServer(app);
+const bare = createBareServer('/api/b/');
+
+app.use((req, res, next) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    //  index.html for all other requests
+    res.sendFile(path.join(__dirname, 'index.html'));
+  }
+});
+
+server.on('upgrade', (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.destroy();
+  }
+});
+
+server.listen(8080, () => {
+  console.log('Server is listening on port 8080');
+});
